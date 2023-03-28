bd3d6ea0a12360b6d171655c6c780b5cca520e83
darker <darkerros@yandex.ru>
2023-03-28 18:34:08 +0300

    Create Base Consruction

---
+++ src/modules/app/components/app/app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Route, Routes} from "react-router-dom";
+
+export const App = () => {
+  return (
+      <div>
+        <Routes>
+            <Route path={"/"}>
+
+            </Route>
+        </Routes>
+      </div>
+  );
+};
+
