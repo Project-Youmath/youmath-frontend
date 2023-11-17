6fa57558f14fdc5991fc549d56625555ebddc8f3
Liya Vysotskaya <Imredrat@yandex.ru>
2023-11-17 12:32:40 +0300

    feat: create a'back to top' button for the main page, fix: make the 'back to top' button for the section page movable

--- src/pages/main-page/index.tsx
+++ src/pages/main-page/index.tsx
@@ -1,11 +1,13 @@
 import { PopularTaskSection } from "./components/popular-task-section";
 import { PopularCategorySection } from "./components/popular-category-section";
+import ButtonUpContainer from "../../components/ButtonUpContainer";
 
 const MainPage = () => {
   return (
     <>
       <PopularTaskSection />
       <PopularCategorySection />
+      <ButtonUpContainer isFixed={true} />
     </>
   );
 };
